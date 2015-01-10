e207fd993fd54abd98f52b482da79fbbe27ffa8a
Pawel Koziol <pp.koziol@gmail.com>
2015-01-10 12:56:36 +0200

    using b.pawn_ctrl in attack detection

--- attacks.cpp
+++ attacks.cpp
@@ -6,26 +6,11 @@
 int isAttacked( char byColor, S8 sq ) {
 
     /* pawns */
-    if ( byColor == WHITE ) {
-        if ( IS_SQ( sq + SE ) &&
-                isPiece( WHITE, PAWN, sq + SE )
-           )
-            return 1;
-        if ( IS_SQ( sq + SW ) &&
-                isPiece( WHITE, PAWN, sq + SW )
-           )
-            return 1;
-    }
-    else {
-        if ( IS_SQ( sq + NE ) &&
-                isPiece( BLACK, PAWN, sq + NE )
-           )
-            return 1;
-        if ( IS_SQ( sq + NW ) &&
-                isPiece( BLACK, PAWN, sq + NW )
-           )
-            return 1;
-    }
+    if ( byColor == WHITE && b.pawn_ctrl[WHITE][sq] )
+         return 1;
+    
+	if ( byColor == BLACK && b.pawn_ctrl[BLACK][sq])
+		 return 1;
 
     /* knights */
     if ( leaperAttack( byColor, sq, KNIGHT ) )
@@ -36,20 +21,18 @@ int isAttacked( char byColor, S8 sq ) {
         return 1;
 
     /* straight line sliders */
-    if ( straightAttack( byColor, sq, NORTH ) ||
-            straightAttack( byColor, sq, SOUTH ) ||
-            straightAttack( byColor, sq, EAST  ) ||
-            straightAttack( byColor, sq, WEST  )
-       )
-        return 1;
+    if ( straightAttack( byColor, sq, NORTH ) 
+	||   straightAttack( byColor, sq, SOUTH ) 
+	||   straightAttack( byColor, sq, EAST  ) 
+	||   straightAttack( byColor, sq, WEST  ) )
+         return 1;
 
     /* diagonal sliders */
-    if ( diagAttack( byColor, sq, NE ) ||
-            diagAttack( byColor, sq, SE ) ||
-            diagAttack( byColor, sq, NW ) ||
-            diagAttack( byColor, sq, SW )
-       )
-        return 1;
+    if ( diagAttack( byColor, sq, NE ) 
+	||   diagAttack( byColor, sq, SE ) 
+	||   diagAttack( byColor, sq, NW ) 
+	||   diagAttack( byColor, sq, SW ) )
+         return 1;
 
     return 0;
 }
@@ -58,9 +41,8 @@ int leaperAttack( char byColor, S8 sq, char byPiece ) {
     S8 nextSq;
     for ( int dir = 0; dir < 8; dir++ ) {
         nextSq = sq + vector[byPiece][dir];
-        if ( IS_SQ(nextSq) &&
-                isPiece( byColor, byPiece, nextSq )
-           )
+        if ( IS_SQ(nextSq) 
+		&&  isPiece( byColor, byPiece, nextSq ) )
             return 1;
     }
     return 0;
@@ -72,15 +54,13 @@ int straightAttack(char byColor, S8 sq, int vect) {
 
     while ( IS_SQ(nextSq) ) {
         if (b.color[nextSq] != COLOR_EMPTY ) {
-            if ( ( b.color[nextSq] == byColor ) &&
-                    ( b.pieces[nextSq] == ROOK || b.pieces[nextSq] == QUEEN )
-               )
-                return 1;
+            if ( ( b.color[nextSq] == byColor ) 
+			&&   ( b.pieces[nextSq] == ROOK || b.pieces[nextSq] == QUEEN ) )
+               return 1;
             return 0;
         }
         nextSq = nextSq + vect;
     }
-
     return 0;
 }
 
@@ -90,14 +70,12 @@ int diagAttack(int byColor, S8 sq, int vect) {
 
     while ( IS_SQ( nextSq ) ) {
         if (b.color[ nextSq ] != COLOR_EMPTY ) {
-            if ( ( b.color[nextSq] == byColor ) &&
-                    ( b.pieces[nextSq] == BISHOP || b.pieces[nextSq] == QUEEN )
-               )
-                return 1;
+            if ( ( b.color[nextSq] == byColor ) 
+			&&   ( b.pieces[nextSq] == BISHOP || b.pieces[nextSq] == QUEEN ) )
+               return 1;
             return 0;
         }
         nextSq = nextSq + vect;
     }
-
     return 0;
 }
